37df19b22c532eadd605c1210955d092d42a0ff4
ruined-1 <zachruined@gmail.com>
2026-04-30 20:10:47 -0400

    Create deploy-commands.js

---
+++ deploy-commands.js
@@ -0,0 +1,28 @@
+const { REST, Routes } = require('discord.js');
+const fs = require('fs');
+require('dotenv').config();
+
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+    try {
+        console.log('Started refreshing application (/) commands.');
+
+        await rest.put(
+            Routes.applicationGuildCommands(process.env.CLIENT_ID, '1139943473437495457'),
+            { body: commands },
+        );
+
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        console.error(error);
+    }
+})();
