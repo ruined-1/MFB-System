4a8699257fc9abe2613714af463ccec42d6d0108
ruined-1 <zachruined@gmail.com>
2026-05-02 18:21:32 +0000

    Update severity.js

--- dupe/severity.js
+++ dupe/severity.js
@@ -1,21 +1,28 @@
 // severity.js
 
+// Editable severity levels
+export const severityLevels = {
+    YELLOW: 20,
+    ORANGE: 50,
+    RED: 100
+};
+
+export function setSeverityLevel(level, value) {
+    severityLevels[level] = value;
+}
+
 export function getSeverityLabel(amountOwned) {
-    if (amountOwned >= 100) return "RED";
-    if (amountOwned >= 50) return "ORANGE";
-    if (amountOwned >= 20) return "YELLOW";
+    if (amountOwned >= severityLevels.RED) return "RED";
+    if (amountOwned >= severityLevels.ORANGE) return "ORANGE";
+    if (amountOwned >= severityLevels.YELLOW) return "YELLOW";
     return "LOW";
 }
 
 export function getSeverityColor(severity) {
     switch (severity) {
-        case "RED":
-            return 0xff0000;
-        case "ORANGE":
-            return 0xffa500;
-        case "YELLOW":
-            return 0xffff00;
-        default:
-            return 0x00ff00;
+        case "RED": return 0xff0000;
+        case "ORANGE": return 0xffa500;
+        case "YELLOW": return 0xffff00;
+        default: return 0x00ff00;
     }
 }
