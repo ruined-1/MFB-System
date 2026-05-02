19d471f09ec6604c3cf71bb39027d50561a739bc
ruined-1 <zachruined@gmail.com>
2026-05-01 20:22:29 -0400

    Update alerts.js

--- dupe/alerts.js
+++ dupe/alerts.js
@@ -9,8 +9,8 @@ import { startCooldown, isOnCooldown, getRemaining } from "./cooldowns.js";
 import { getSeverityColor, getSeverityLabel } from "./severity.js";
 
 const ALERT_CHANNEL_ID = "1496324911084470473";
-const NORMAL_PING = "967946056572747776";
-const ESCALATION_PING = "750441339195490335";
+const NORMAL_PING = "";
+const ESCALATION_PING = "";
 
 export default async function alertHandler(msg, client) {
     try {
