9fcefd22909e2a4cbc37d753bf4f4afa6a991330
ruined-1 <zachruined@gmail.com>
2026-05-02 17:53:54 -0400

    Create cooldownAPI.js

---
+++ commands/cooldownAPI.js
@@ -0,0 +1,15 @@
+import {
+  isOnCooldown,
+  getCooldownEnd,
+  setCooldownEnd,
+  resetCooldown,
+  getRemaining
+} from "./cooldowns.js";
+
+export default {
+  isOnCooldown,
+  getCooldownEnd,
+  setCooldownEnd,
+  resetCooldown,
+  getRemaining
+};
