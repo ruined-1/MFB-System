82ce26c99563e4cfe871d8858d3e0a475d443352
ruined-1 <zachruined@gmail.com>
2026-05-02 18:21:20 +0000

    Create threshold.js

---
+++ dupe/threshold.js
@@ -0,0 +1,11 @@
+// threshold.js
+
+let threshold = 20; // default threshold
+
+export function getThreshold() {
+    return threshold;
+}
+
+export function setThreshold(value) {
+    threshold = value;
+}
