086567970df7ebd5e5b89aecc94eee1322adefec
ruined-1 <zachruined@gmail.com>
2026-05-02 22:30:28 -0400

    Create resetCooldown.js

---
+++ dupe_DISABLED/resetCooldown.js
@@ -0,0 +1,13 @@
+// resetCooldown.js
+export default async function resetCooldown(interaction, client) {
+  if (!interaction.isButton()) return;
+
+  const id = interaction.customId.replace("reset_", "");
+
+  client.cooldownSystem.resetCooldown(id);
+
+  await interaction.reply({
+    content: `Cooldown reset for player ID **${id}**.`,
+    ephemeral: true
+  });
+}
