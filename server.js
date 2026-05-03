234f5e92f239e28856bc97389a6ecad76117f31f
ruined-1 <zachruined@gmail.com>
2026-05-03 09:07:59 -0400

    Delete server.js

--- server.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.get("/", (req, res) => res.send("Bot is running"));
-app.listen(PORT, () => console.log(`Web server running on port ${PORT}`));
